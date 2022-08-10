190dd8ef7d20a18825a35af1e924c926b99c844e
daviroo <david.borthwick@gmail.com>
2022-08-10 15:40:40 +0100

    feat: Improved countdown timer hook

--- src/custom-hooks/use-countdown-timer.ts
+++ src/custom-hooks/use-countdown-timer.ts
@@ -1,27 +1,83 @@
 import { useEffect, useState } from "react"
 
-export const useCountdownTimer = (
-  seconds: number,
-  completedCallback?: void | (() => void),
-) => {
-  const [timeLeft, setTimeLeft] = useState(seconds)
+type useCountdownTimerReturnValue = {
+  timeLeft: number | undefined
+  startCountdownTimer: (seconds: number, callback?: () => void) => void
+  stopCountdownTimer: () => void
+  resetCountdownTimer: (seconds: number, newCallback?: () => void) => void
+  resetCountdownTimerAndExecuteCallback: (
+    seconds: number,
+    newCallback?: () => void,
+  ) => void
+  stopCountdownTimerAndExecuteCallback: (newCallback?: () => void) => void
+}
+
+export const useCountdownTimer = (): useCountdownTimerReturnValue => {
+  const [timeLeft, setTimeLeft] = useState<number>()
+  const [completedCallback, setCompletedCallback] = useState<() => void>()
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTimeLeft(timeLeft - 1)
-    }, 1000)
+    if (timeLeft !== undefined) {
+      const interval = setInterval(() => {
+        setTimeLeft(timeLeft - 1)
+      }, 1000)
 
-    if (timeLeft === 0) {
-      clearInterval(interval)
-      if (completedCallback) {
-        completedCallback()
+      if (timeLeft === 0) {
+        clearInterval(interval)
+        if (completedCallback) {
+          completedCallback()
+        }
       }
-    }
 
-    return () => {
-      clearInterval(interval)
+      return () => {
+        clearInterval(interval)
+      }
     }
   }, [timeLeft, completedCallback])
 
-  return timeLeft
+  const startCountdownTimer = (seconds: number, callback?: () => void) => {
+    setTimeLeft(seconds)
+    if (callback) {
+      setCompletedCallback(() => callback)
+    }
+  }
+
+  const stopCountdownTimer = () => {
+    setTimeLeft(0)
+  }
+
+  const resetCountdownTimer = (seconds: number, newCallback?: () => void) => {
+    setTimeLeft(seconds)
+    if (newCallback) {
+      setCompletedCallback(() => newCallback)
+    }
+  }
+
+  const resetCountdownTimerAndExecuteCallback = (
+    seconds: number,
+    newCallback?: () => void,
+  ) => {
+    setTimeLeft(seconds)
+    if (completedCallback) {
+      completedCallback()
+    }
+    setCompletedCallback(() => newCallback)
+  }
+
+  const stopCountdownTimerAndExecuteCallback = (newCallback?: () => void) => {
+    setTimeLeft(0)
+    if (completedCallback) {
+      completedCallback()
+    }
+    setCompletedCallback(() => newCallback)
+  }
+
+  return {
+    timeLeft,
+    startCountdownTimer,
+    stopCountdownTimer,
+    resetCountdownTimer,
+    resetCountdownTimerAndExecuteCallback,
+    stopCountdownTimerAndExecuteCallback,
+  }
 }
